ab8c535690912ead168fa32d2e1956c48067b1b6
stas.ostapenko@gmail.com <stas.ostapenko@gmail.com>
2018-04-03 22:54:17 +0300

    logs reader moved to service

--- app.js
+++ app.js
@@ -20,20 +20,4 @@ var service = require('./service.js')(client);
 //service.getLatestBalance(addr);
 
 var contractAddr = '0xc3ec0a63e400dcfc200e09d1d0a03610b4f7260d';
-service.newFilter(contractAddr)
-.then(function (filterIdJson) {
-    console.log('new filter : '+JSON.stringify(filterIdJson,null,'\t'));
-    return Q(filterIdJson.result);
-}).then(function (filterId) {
-    console.log('filterID : '+JSON.stringify(filterId,null,'\t'));
-    console.log("Sleeping 10 sec....");
-    return Q.all([Q.delay(10000),filterId])
-}).spread(function(delayResult,filterId){
-    console.log("grabbing events for filerID : "+filterId);
-    return Q.all([service.getFilterLogs(filterId),filterId]);
-}).spread(function (result, filterId) {
-    console.log("filter changes (logs) : "+JSON.stringify(result,null,'\t'));
-    return service.uninstallFilter(filterId);
-}).then(function (value) {
-    console.log("Filter uninstall response : "+JSON.stringify(value,null,'\t'));
-}).done();
+service.getEventsByAddress(contractAddr);
--- service.js
+++ service.js
@@ -1,11 +1,12 @@
 const log = require('winston');
 const ETH_RPC_API = require('./eth-rpc-api.js');
-module.exports = function(client){
+var Q = require('q');
+module.exports = function (client) {
     var external = {};
-    external.getSyncStatus = function() {
+    external.getSyncStatus = function () {
         client.call(ETH_RPC_API.eth_syncing)
             .then(function (response) {
-                if(response.result == false) {
+                if (response.result == false) {
                     log.info("Sync completed");
                     return;
                 }
@@ -24,26 +25,47 @@ module.exports = function(client){
         }).done();
     };
     external.getLatestBalance = function (addr) {
-        return client.call(ETH_RPC_API.eth_getBalance(addr,"0x0"))
+        return client.call(ETH_RPC_API.eth_getBalance(addr, "0x0"))
             .then(function (balance) {
-                log.info("Latest balance : "+JSON.stringify(balance,null,'\t'));
+                log.info("Latest balance : " + JSON.stringify(balance, null, '\t'));
             }).done();
     };
 
-    external.newFilter = function (address){
+    external.newFilter = function (address) {
         return client.call(ETH_RPC_API.eth_newFilter(address))
     };
 
-    external.getFilterChanges = function(filterId){
+    external.getFilterChanges = function (filterId) {
         return client.call(ETH_RPC_API.eth_getFilterChanges(filterId));
     };
 
-    external.getFilterLogs = function(filterId){
+    external.getFilterLogs = function (filterId) {
         return client.call(ETH_RPC_API.eth_getFilterLogs(filterId));
     };
 
-    external.uninstallFilter = function(filterId){
+    external.uninstallFilter = function (filterId) {
         return client.call(ETH_RPC_API.eth_uninstallFilter(filterId));
+    };
+
+    //don't care about logging level at this moment
+    external.getEventsByAddress = function (addr) {
+        external.newFilter(addr)
+        .then(function (filterIdJson) {
+                log.info('new filter : ' + JSON.stringify(filterIdJson, null, '\t'));
+                return Q(filterIdJson.result);
+            }).then(function (filterId) {
+            log.info('filterID : ' + JSON.stringify(filterId, null, '\t'));
+            log.info("Sleeping 10 sec....");
+            return Q.all([Q.delay(10000), filterId])
+        }).spread(function (delayResult, filterId) {
+            log.info("grabbing events for filerID : " + filterId);
+            return Q.all([external.getFilterLogs(filterId), filterId]);
+        }).spread(function (result, filterId) {
+            log.info("filter changes (logs) : " + JSON.stringify(result, null, '\t'));
+            return external.uninstallFilter(filterId);
+        }).then(function (value) {
+            log.info("Filter uninstall response : " + JSON.stringify(value, null, '\t'));
+        }).done();
     };
     return external;
 };
